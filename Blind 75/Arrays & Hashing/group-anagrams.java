36b5920d7e16ab3aa372e437f3bf2c31ae52a283
Giacomo Motta <99472580+giacomib@users.noreply.github.com>
2025-11-03 11:56:37 +0100

    Add files via upload

---
+++ Blind 75/Arrays & Hashing/group-anagrams.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> res = new ArrayList<>();
+        Map<String, ArrayList<String>> map = new HashMap<>();
+
+        for (String curr : strs) {
+            char[] charCurr = curr.toCharArray();
+            Arrays.sort(charCurr);
+            String tmp = new String(charCurr);
+            if (!map.containsKey(tmp))
+                map.put(tmp, new ArrayList<>());
+            map.get(tmp).add(curr);
+        }
+
+        for (var entry : map.entrySet()) {
+            ArrayList<String> val = entry.getValue();
+            res.add(new ArrayList<>(val));
+        }
+        return res;
+    }
+}
